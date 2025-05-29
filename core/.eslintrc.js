ffbda828aa1c43d7e719f9827b0acbe2d2b72281
fpdevel <fpdevelgroup@gmail.com>
2025-05-29 22:47:54 +0200

    fix bug on sharp

--- core/.eslintrc.js
+++ core/.eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  extends: 'airbnb',
-  globals: {
-    fetch: false,
-    test: false,
-    expect: false,
-  },
-  rules: {
-    'no-confusing-arrow': 'off',
-    'implicit-arrow-linebreak': 'off',
-    indent: 'off',
-    'react/jsx-filename-extension': 'off',
-    'import/no-unresolved': 'off',
-    'operator-linebreak': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
-  },
-  ignorePatterns: ['lib/**'],
+    extends: 'airbnb',
+    globals: {
+        fetch: false,
+        test: false,
+        expect: false,
+    },
+    rules: {
+        'no-confusing-arrow': 'off',
+        'implicit-arrow-linebreak': 'off',
+        indent: 'off',
+        'react/jsx-filename-extension': 'off',
+        'import/no-unresolved': 'off',
+        'operator-linebreak': 'off',
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'padded-blocks': 'off',
+    },
+    ignorePatterns: ['lib/**'],
 };
